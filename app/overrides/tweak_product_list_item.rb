5c793188909c26a481849d752990a3869fcc3b48
Dejan <dpantic@gmail.com>
2012-04-26 18:20:42 +0200

    update screen orange color

--- app/overrides/tweak_product_list_item.rb
+++ app/overrides/tweak_product_list_item.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_products',
                      :name => 'tweak_product_list_item',
                      :replace => '[data-hook="products_list_item"]',
-                     :text => %q{ <li id="product_<%= product.id %>" data-hook="products_list_item">
+                     :text => %q{ <Ï="product_<%= product.id %>" data-hook="products_list_item">
                                     <%= link_to(product, :class => 'info') do %>
                                       <%= product.name %>
                                       <span class="price selling"><%= number_to_currency product.price %></span>
